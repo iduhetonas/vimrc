b831d3c39f6acb333910fcd6046a1718315dfd5f
Tyler Huffman <tyler.huffman@tylerh.org>
2013-03-15 13:44:32 -0600

    Reordered some spots in the main.vim file

--- install_vimrc.sh
+++ install_vimrc.sh
@@ -3,5 +3,6 @@ runtime $PWD/bundle/vim-pathogen/autoload/pathogen.vim
 
 source $PWD/vim_config/main.vim
 source $PWD/vim_config/filetypes.vim
+source $PWD/vim_config/plugins.vim
 " > $HOME/.vimrc
 echo "Vimrc installed!"
