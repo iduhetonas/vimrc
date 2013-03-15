812d82b854fb29540d4db7ab82bdcc5b47451b32
Tyler Huffman <tyler.huffman@tylerh.org>
2013-03-15 11:30:42 -0600

    Added pathogen and allowed for module loading

--- install_vimrc.sh
+++ install_vimrc.sh
@@ -1,4 +1,6 @@
 echo "set runtimepath+=$PWD
+runtime $PWD/bundle/vim-pathogen/autoload/pathogen.vim
 
 source $PWD/vim_config/main.vim
-source $PWD/vim_config/filetypes.vim" > $HOME/.vimrc
+source $PWD/vim_config/filetypes.vim
+" > $HOME/.vimrc
