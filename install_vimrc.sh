3c875c27b99fd1d496c01f56ae2048f3e46cc3dc
iduhetonas <tyler.huffman@tylerh.org>
2014-04-23 21:46:20 -0600

    Added YouCompleteMe as well as an install and build script for it

--- install_vimrc.sh
+++ install_vimrc.sh
@@ -7,11 +7,17 @@ source $PWD/vim_config/filetypes.vim
 source $PWD/vim_config/plugins.vim
 " > $HOME/.vimrc
 git submodule init
-git submodule update
+git submodule update --init --recursive
+
+# This installs YouCompleteMe
+# TODO: Make sure that the Vim version is correct
+cd bundle/youcompleteme
+sudo apt-get install build-essential cmake python-dev
+source install.sh --clang-completer --omnisharp-completer
 
 if hash cabal 2>/dev/null; then
     # Install ghcmod
-    cd bundle/ghcmod-vim/
+    cd ../bundle/ghcmod-vim/
     source install-deps.sh
 else
     # Cabal not installed
