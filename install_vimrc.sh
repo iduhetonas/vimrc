168003e6f1b73421dd5cf4f5906f887516adf6ad
Tyler Huffman <tyler.huffman@tylerh.org>
2014-06-04 10:59:19 -0600

    Reverted to a 'bare-bones' vim setup

--- install_vimrc.sh
+++ install_vimrc.sh
@@ -9,11 +9,4 @@ source $PWD/vim_config/plugins.vim
 git submodule init
 git submodule update --init --recursive
 
-# This installs YouCompleteMe
-# TODO: Make sure that the Vim version is correct
-cd bundle/youcompleteme
-# TODO: Ubuntu-specific
-sudo apt-get install build-essential cmake python-dev
-source install.sh --clang-completer --omnisharp-completer
-
 echo "Vimrc installed!"
